e3dbc42a8cb5c2eb2ad38675a074008a7af0e617
kisel <kiselevaeva377@gmail.com>
2023-11-23 15:51:34 +0300

    without Ui

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import sys
 from random import randint
 
-from PyQt5 import uic
 from PyQt5.QtGui import QColor, QPainter
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
         self.initUI()
         self.qp = QPainter()
         self.do_paint = False
+
     def initUI(self):
+        self.pushButton = QPushButton('Кнопка', self)
+        self.pushButton.resize(100, 50)
+        self.pushButton.move(50, 50)
         self.setGeometry(0, 0, 800, 800)
         self.pushButton.clicked.connect(self.draw_prep)
 
@@ -26,14 +28,13 @@ class MyWidget(QMainWindow):
 
     def draw_circl(self):
         radius = randint(10, 100)
-        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         self.qp.drawEllipse(400 - radius, 400 - radius, radius * 2, radius * 2)
         self.qp.end()
 
     def draw_prep(self):
         self.do_paint = True
         self.repaint()
-
 
 
 if __name__ == '__main__':
